cb6a23d129939063766abd9e25fd7bbcc1902c18
edutjie <edutjitrahardja@gmail.com>
2022-09-02 12:11:21 +0700

    v1 - 40%

--- 00/Lab0.java
+++ 00/Lab0.java
@@ -1,13 +1,25 @@
 import java.io.*;
+import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Lab0 {
     private static InputReader in;
     private static PrintWriter out;
 
-
-    static int multiplyMod(int N, int Mod, int[] x) {
-        return -1;
+    static BigInteger multiplyMod(int N, int Mod, int[] x) {
+        // base case: if N == 0 returns 1
+        if (N == 0)
+            return BigInteger.valueOf(1);
+        /**
+         * recursion case: in each iteration, it will multiply first element of the
+         * array with the element in the next index using recursion and
+         * modulos it using Mod
+         * 
+         * convert return type to BigInteger so it supports the limit from the test case
+         **/
+        return BigInteger.valueOf(x[0]).multiply(multiplyMod(N - 1, Mod, Arrays.copyOfRange(x, 1, N)))
+                .mod(BigInteger.valueOf(Mod));
     }
 
     public static void main(String[] args) throws IOException {
@@ -26,7 +38,7 @@ public class Lab0 {
         }
 
         // TODO: implement method multiplyMod(int, int, int[]) to get the answer
-        int ans = multiplyMod(N, (int) (1e9+7), x);
+        BigInteger ans = multiplyMod(N, (int) (1e9 + 7), x);
         out.println(ans);
 
         // don't forget to close/flush the output
@@ -34,8 +46,10 @@ public class Lab0 {
     }
 
     // taken from https://codeforces.com/submissions/Petr
-    // together with PrintWriter, these input-output (IO) is much faster than the usual Scanner(System.in) and System.out
-    // please use these classes to avoid your fast algorithm gets Time Limit Exceeded caused by slow input-output (IO)
+    // together with PrintWriter, these input-output (IO) is much faster than the
+    // usual Scanner(System.in) and System.out
+    // please use these classes to avoid your fast algorithm gets Time Limit
+    // Exceeded caused by slow input-output (IO)
     static class InputReader {
         public BufferedReader reader;
         public StringTokenizer tokenizer;
