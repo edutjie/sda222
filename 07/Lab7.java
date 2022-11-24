ac44755a0feaaef78c26bb2e023c71da0091fcd2
edutjie <edutjitrahardja@gmail.com>
2022-11-24 23:35:24 +0700

    Lab 06 V1

--- 07/Lab7.java
+++ 07/Lab7.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.*;
 import java.util.StringTokenizer;
 
 public class Lab7 {
@@ -11,27 +12,36 @@ public class Lab7 {
         OutputStream outputStream = System.out;
         out = new PrintWriter(outputStream);
 
+        // N = total jumlah benteng; M = jumlah benteng yang diserang;
         int N = in.nextInt(), M = in.nextInt();
-
-        for (int i = 1; i <= N; i++) {
-            // TODO: Inisialisasi setiap benteng
-        }
+        Graph graph = new Graph(N);
+        // for (int i = 1; i <= N; i++) {
+        // // TODO: Inisialisasi setiap benteng
+        // graph.addEdge(N, M, i);
+        // }
 
         for (int i = 0; i < M; i++) {
-            int F = in.nextInt();
+            int F = in.nextInt(); // nomor benteng yang sedang diserang
             // TODO: Tandai benteng F sebagai benteng diserang
+            graph.adj.get(F - 1).isAttacked = true;
         }
 
-        int E = in.nextInt();
+        int E = in.nextInt(); // jumlah jalan yang terdapat pada Kerajaan Fortdom
         for (int i = 0; i < E; i++) {
+            // A = source; B = destination; W = jumlah musuh yang ada di jalan tersebut
+            // (weight)
             int A = in.nextInt(), B = in.nextInt(), W = in.nextInt();
             // TODO: Inisialisasi jalan berarah dari benteng A ke B dengan W musuh
+            graph.addEdge(A, B, W);
         }
 
-        int Q = in.nextInt();
+        int Q = in.nextInt(); // jumlah query
+        boolean visited[][] = new boolean[5000][5000];
         while (Q-- > 0) {
+            // S = source; K = jumlah orang bala bantuan
             int S = in.nextInt(), K = in.nextInt();
             // TODO: Implementasi query
+            out.println(graph.bfs(S, K, visited, out) ? "YES" : "NO");
         }
 
         out.close();
@@ -66,5 +76,84 @@ public class Lab7 {
             return Integer.parseInt(next());
         }
 
+    }
+}
+
+class Pair {
+    // first = source; second = jumlah bala bantuan
+    int first, second;
+
+    public Pair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+}
+
+class Edge {
+    // from = source; to = destination; weight = jumlah musuh
+    int from, to, weight;
+
+    public Edge(int from, int to, int weight) {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+    }
+}
+
+class Vertex {
+    LinkedList<Edge> edges;
+    boolean isAttacked;
+
+    public Vertex() {
+        this.edges = new LinkedList<Edge>();
+        this.isAttacked = false;
+    }
+}
+
+class Graph {
+    // referensi:
+    // https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
+
+    public int V;
+    public ArrayList<Vertex> adj;
+
+    public Graph(int v) {
+        this.V = v;
+        this.adj = new ArrayList<>();
+        for (int i = 1; i <= v; i++)
+            this.adj.add(new Vertex());
+    }
+
+    public void addEdge(int v, int w, int weight) {
+        this.adj.get(v - 1).edges.add(new Edge(v, w, weight));
+    }
+
+    public boolean bfs(int src, int troops, boolean[][] visited, PrintWriter out) {
+        ArrayDeque<Pair> queue = new ArrayDeque<>();
+        queue.add(new Pair(src, troops));
+        while (!queue.isEmpty()) {
+            Pair curr = queue.poll();
+            int currSrc = curr.first;
+            int currTroops = curr.second;
+            if (currTroops <= 0)
+                continue;
+            // out.println("======================================================");
+            // out.println("currSrc: " + currSrc + " currTroops: " + currTroops);
+            if (this.adj.get(currSrc - 1).isAttacked)
+                return true;
+            visited[currSrc - 1][currTroops - 1] = true;
+            for (Edge edge : this.adj.get(currSrc - 1).edges) {
+                int nextSrc = edge.to;
+                int enemy = edge.weight;
+                if (!visited[nextSrc - 1][currTroops - 1]) {
+                    // out.println("nextSrc: " + nextSrc + " enemy: " + enemy);
+                    if (currTroops - enemy > 0)
+                        queue.add(new Pair(nextSrc, currTroops - enemy));
+                }
+            }
+            // out.println("END");
+        }
+
+        return false;
     }
 }
